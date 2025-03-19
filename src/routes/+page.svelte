5f6d728987d6cb68946c02a9c63396f6efca1bc6
hasuburero <hasuburero0802@gmail.com>
2025-03-19 14:42:19 +0900

    almost first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	type Item = {
+		name: string;
+	};
+
+	let withList: Item[] = [{ name: 'Apple' }, { name: 'Orange' }];
+</script>
+
+<section>
+	<div>
+		<h1>✅ With List</h1>
+		<div>
+			<input type="text" />
+			<button>Add Item</button>
+		</div>
+		<ul>
+			{#each withList as item}
+				<li>
+					<p><span>✔</span><span>{item.name}</span></p>
+					<button>🗑️</button>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</section>
