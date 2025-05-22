b2bf3f03bc2aa0d92089c15340a7edd7c357aa9e
hasuburero <hasuburero0802@gmail.com>
2025-05-22 18:14:11 +0900

    adding test code wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
-	type Item = {
+	interface Item {
 		name: string;
-	};
+	}
 
 	let withList: Item[] = [{ name: 'Apple' }, { name: 'Orange' }];
+
+	let adding_item: Item = { name: '' };
+	function AddItem() {
+		if (adding_item.name === 'Svelte') {
+			console.log('This is Svelte');
+		} else if (adding_item.name == 'Hello') {
+			console.log('Hello World!!');
+		} else {
+			console.log('other');
+		}
+		withList.push(adding_item);
+		console.log(withList);
+		adding_item.name = '';
+	}
 </script>
 
-<section>
+<main>
 	<div>
 		<h1>✅ With List</h1>
+		<p>
+			Visit the <a href="http://www.dlab.ise.shibaura-it.ac.jp/nextcloud">NextCloud</a> !!
+		</p>
 		<div>
-			<input type="text" />
-			<button>Add Item</button>
+			<input bind:value={adding_item.name} type="text" />
+			<button on:click={AddItem}>Add Item</button>
 		</div>
 		<ul>
 			{#each withList as item}
@@ -22,4 +39,19 @@
 			{/each}
 		</ul>
 	</div>
-</section>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
